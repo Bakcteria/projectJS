4df8f3f0ff4b66cac2411d7b277df2545835b52a
Andrey <samarin.andru@gmail.com>
2024-07-24 08:19:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+var leftBoarderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+//console.log(4/0);
+//console.log('string'*9);
+
+let person = {
+    name: "Jhon",
+    age: 19,
+    isMaried: false
+}
+
+console.log(person.name);
+//alert("Hello World!");
+console.log(person.age);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+//let answer1 = confirm("Are you here?");
+
+//console.log(answer);
+
+//let answer2 = prompt("Есть ли вам 18", "Yes");
+
+//console.log(answer2);
+
+console.log("Andrey " + 5);
