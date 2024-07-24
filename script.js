dc7130bd5c90e21da55effcf3a5132590c8b2bbe
Andrey <samarin.andru@gmail.com>
2024-07-24 22:48:15 +0300

    UpdateHomeWork

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 let money = prompt("Ваш бюджет на месяц?");
 let date = prompt("Введите дату в формате YYYY-MM-DD");
 
+let appData = {
+    budget: money,
+    timeData: date,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    saving: false
+}
+
+let quest1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    quest2 = prompt("Во сколько обойдется?", ''),
+    quest3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    quest4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.quest1 = quest2;
+appData.expenses.quest3 = quest4;
+
+alert(appData.expenses/30);
