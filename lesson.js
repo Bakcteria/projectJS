4cd784f96ba0ddecffad19af3b71da57b7044403
Andrey <samarin.andru@gmail.com>
2024-07-28 23:18:46 +0300

    Update lesson.js

--- lesson.js
+++ lesson.js
@@ -1,21 +1,20 @@
-function first() {
-    setTimeout( function() {
-        console.log(1);
-    }, 500 )
+let options = {
+    width: 1024,
+    height: 1024,
+    name: "test"
+};
+
+console.log(options.name);
+options.bool = false;
+options.colors = {
+    border: "black",
+    bg: "red"
+};
+
+delete options.bool;
+
+console.log(options);
+
+for (let key in options){
+    console.log('Сыойства ' + key + ' имеет значение ' + options[key]);
 }
-
-function second() {
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-    console.log("Я учу " + lang);
-    callback();
-}
-
-learnJS("JavaScript", function(){
-    console.log("Я прошёл третий урок");
-})
