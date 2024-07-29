9d1b49d983c93dc799e8d7fe3925b7c230f3b188
Andrey <samarin.andru@gmail.com>
2024-07-29 09:41:03 +0300

    Update lesson.js

--- lesson.js
+++ lesson.js
@@ -18,3 +18,15 @@ console.log(options);
 for (let key in options){
     console.log('Сыойства ' + key + ' имеет значение ' + options[key]);
 }
+
+let arr = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+arr.forEach(function(item, i, mass) {
+    console.log(i + ': ' + item + " (массив: " + mass + ')');
+});
+
+console.log(arr);
