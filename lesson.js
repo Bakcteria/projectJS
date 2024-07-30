beee485c4a383f08152edbca0303d216df77e7b4
Andrey <samarin.andru@gmail.com>
2024-07-30 09:52:22 +0300

    Update lesson.js

--- lesson.js
+++ lesson.js
@@ -30,3 +30,17 @@ arr.forEach(function(item, i, mass) {
 });
 
 console.log(arr);
+
+let soldier = {
+    health: 400,
+    armor: 100
+}
+
+let john = {
+    health: 100
+}
+
+john.__proto__ = soldier;
+console.log(john);
+console.log(john.armor);
+console.log(john.health);
